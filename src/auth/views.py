0ae71ee43a22f08939a951af14b13740ae6a2ece
chvaldez10 <christianvaldez021@gmail.com>
2024-11-04 22:23:13 -0700

    create login view

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
 # Create your views here.
+def login_view(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+    
+    return render(request, "auth/login.html", {})
+
+# def register_view(request):
+#     return render(request, "auth/register.html", {})
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from auth import views as auth_views
 from .views import home_page_view
 
 urlpatterns = [
     path('', home_page_view, name='home'),
     path("hello-world/", home_page_view),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.login_view, name='login'),
 ]
