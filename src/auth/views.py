787f20929c147ef2b540c9fbe5767e0ea6c11797
chvaldez10 <christianvaldez021@gmail.com>
2024-11-11 16:57:03 -0700

    add login view

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,15 +1,25 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def login_view(request):
-    username = request.POST.get("username")
-    password = request.POST.get("password")
-    user = authenticate(request, username=username, password=password)
-
-    if user is not None:
-        login(request, user)
+    if request.user.is_authenticated:
+        return redirect("/")
     
+    if request.method == "POST":
+        username = request.POST.get("username") or None
+        password = request.POST.get("password") or None
+
+        if all([username, password]):
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                print("User authenticated")
+                login(request, user)
+                return redirect("/")
+
+            print(f"User is authenticated: {request.user.is_authenticated}")
+
     return render(request, "auth/login.html", {})
 
 # def register_view(request):
